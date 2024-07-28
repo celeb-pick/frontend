2cfb60de9967e1a98d3554bc9576d734fba593fb
Eunchae Kim <123533586+lucaseunchae@users.noreply.github.com>
2024-07-28 14:52:55 +0900

    chore: linter, formatter 설정 (#4)
    * chore(package): ESLint Airbnb 패키지 추가
    
    * chore(eslint-config): ESLint 설정 추가
    
    * chore(package): Prettier 관련 패키지 추가
    
    * chore(prettier-config): Prettier 설정 추가
    
    * style: 코드 스타일 포맷팅
    
    * chore(CI): CI 과정에 lint 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,26 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
+  },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
-}
+};
