dd853f5ba947a013066a3bf23108070e3e444bf7
Eunchae Kim <kec9706@gmail.com>
2024-08-02 21:24:34 +0900

    story: Chip 컴포넌트 스토리 작성

--- src/components/atoms/chip/chip.tsx
+++ src/components/atoms/chip/chip.tsx
@@ -3,8 +3,20 @@ import tw, { css } from 'twin.macro';
 import { tailwindColors } from '../../../constants/tailwind';
 
 interface ChipProps extends ComponentPropsWithoutRef<'div'> {
+  /**
+   * 칩에 나타낼 텍스트 데이터 입니다.
+   */
   label: string;
+
+  /**
+   * 칩의 형태로 `outlined`는 border 스타일의 칩을,
+   * `filled`는 background-color가 채워진 스타일의 칩의 형태를 나타냅니다.
+   */
   variant?: 'outlined' | 'filled';
+
+  /**
+   * 칩의 색상으로 Tailwind Color에 기반하고 있습니다.
+   */
   color?: keyof typeof tailwindColors;
 }
 
@@ -28,6 +40,9 @@ const fillStyle = (color: ChipProps['color']) => {
   `;
 };
 
+/**
+ * 데이터를 칩의 형태로 나타내는 컴포넌트 입니다.
+ */
 function Chip({
   label,
   variant = 'outlined',
