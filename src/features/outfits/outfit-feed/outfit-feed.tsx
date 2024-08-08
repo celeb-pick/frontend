f4b58dab7b33ff5e779458cf549cda990b2f916a
Eunchae Kim <kec9706@gmail.com>
2024-08-08 23:10:54 +0900

    story: OutfitFeed 컴포넌트 스토리 작성

--- src/features/outfits/outfit-feed/outfit-feed.tsx
+++ src/features/outfits/outfit-feed/outfit-feed.tsx
@@ -10,11 +10,19 @@ import OutfitFeedPostLoader from './outfit-feed-post-loader';
 import OutfitPostList from './outfit-post-list';
 
 interface OutfitFeedProps extends ComponentPropsWithoutRef<'div'> {
+  /**
+   * Tanstack Query `useSuspenseInfiniteQuery` 메서드의
+   * 호출값을 주입 받습니다.
+   */
   suspenseInfiniteQueryResult: UseSuspenseInfiniteQueryResult<
     InfiniteData<OutfitPostListResponse>
   >;
 }
 
+/**
+ * 홈 페이지, 검색 페이지 등 다양한 곳에서 코디 피드 컴포넌트를
+ * 구현 하는데 사용되는 컴포넌트 입니다.
+ */
 function OutfitFeed({
   suspenseInfiniteQueryResult,
   ...props
