230ee25c4691bf8edbabaa57b7ca1d41849f1893
Eunchae Kim <kec9706@gmail.com>
2024-08-09 04:08:21 +0900

    design: AppBar 컴포넌트 전체적인 크기 축소

--- src/components/common/layout/app-bar.tsx
+++ src/components/common/layout/app-bar.tsx
@@ -5,10 +5,10 @@ function AppBar() {
   return (
     <div
       css={[
-        tw`flex-y-center h-20 px-5 border-b border-solid border-gray-100 bg-white`,
+        tw`flex-y-center h-16 px-4 border-b border-solid border-gray-100 bg-white`,
       ]}
     >
-      <Link to="/" css={[tw`font-semibold text-2xl py-4`]}>
+      <Link to="/" css={[tw`font-semibold text-xl py-4`]}>
         celeb pick
       </Link>
     </div>
