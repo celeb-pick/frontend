65cd7d8078f268b85b898ed313c9c21429e5861c
Eunchae Kim <kec9706@gmail.com>
2024-08-12 16:59:11 +0900

    story: RadioGroup 컴포넌트 스토리 작성

--- src/components/atoms/radio-group/radio-group.tsx
+++ src/components/atoms/radio-group/radio-group.tsx
@@ -6,13 +6,36 @@ import { RadioGroupOptionsType, RadioGroupValueType } from './types';
 type InputProps = ComponentPropsWithoutRef<'input'>;
 
 interface RadioGroupProps {
+  /**
+   * 컴포넌트의 value값 입니다.
+   */
   value: RadioGroupValueType;
+
+  /**
+   * 컴포넌트의 value값이 변할 때 실행 되는 콜백 함수 입니다.
+   */
   onChange: InputProps['onChange'];
+
+  /**
+   * 라벨 텍스트 입니다.
+   */
   label: string;
+
+  /**
+   * `value`와 선택값인 `label`로 이루어진 배열 입니다.
+   * `label`이 없으면 `value`값이 UI에 나타나게 됩니다.
+   */
   options: RadioGroupOptionsType;
+
+  /**
+   * 컴포넌트의 Wrapper 스타일을 지정할 수 있습니다.
+   */
   className?: string;
 }
 
+/**
+ * `input[type='radio']`와 `label`로 이루어진 라디오 그룹 컴포넌트 입니다.
+ */
 function RadioGroup({
   value,
   onChange,
