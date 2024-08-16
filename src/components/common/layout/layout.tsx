6eb49dc7ffd51831d19b43c164fd2166c2257476
Eunchae Kim <kec9706@gmail.com>
2024-08-16 20:44:36 +0900

    design: Layout 컴포넌트 내부 컨텐츠가 height를 꽉 채울 수 있도록 Layout 컴포넌트에 flex-col 속성 추가

--- src/components/common/layout/layout.tsx
+++ src/components/common/layout/layout.tsx
@@ -22,7 +22,7 @@ function Layout({ children, ...props }: LayoutProps) {
   return (
     <div
       css={[
-        tw`mx-auto min-h-[100vh] w-full min-w-[--layout-min-width] max-w-[--layout-max-width] border-x-2 border-solid border-gray-100`,
+        tw`flex flex-col min-h-[100vh] w-full min-w-[--layout-min-width] max-w-[--layout-max-width] mx-auto border-x-2 border-solid border-gray-100`,
       ]}
       {...props}
     >
