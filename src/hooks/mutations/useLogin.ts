c3f4d75eee8f264c67dd2a9bfcf65fa5cf80f2f2
Eunchae Kim <kec9706@gmail.com>
2024-08-16 20:45:54 +0900

    chore: 에러 바운더리를 사용하기 알맞게 관련 설정 변경

--- src/hooks/mutations/useLogin.ts
+++ src/hooks/mutations/useLogin.ts
@@ -1,7 +1,9 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { HttpStatusCode } from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../../api/auth';
 import { LoginRequest } from '../../types/auth';
+import { getServerErrorResponse } from '../../utils/error';
 
 const useLogin = () => {
   const queryClient = useQueryClient();
@@ -9,6 +11,12 @@ const useLogin = () => {
 
   return useMutation({
     mutationFn: (req: LoginRequest) => login(req),
+    throwOnError: (error) => {
+      if (getServerErrorResponse(error)?.status === HttpStatusCode.BadRequest) {
+        return false;
+      }
+      return true;
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['fetchAuthStatus'] });
       // TODO: 내 프로필 페이지로 이동하게 변경
