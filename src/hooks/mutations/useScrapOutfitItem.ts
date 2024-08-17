4562b5afdfac1715e9a92e85821b39555f85e19b
Eunchae Kim <kec9706@gmail.com>
2024-08-18 01:26:38 +0900

    feat: 스크랩 토글시 400, 409에러가 발생하면 ErrorBoundary로 error throw 하지 않도록 변경

--- src/hooks/mutations/useScrapOutfitItem.ts
+++ src/hooks/mutations/useScrapOutfitItem.ts
@@ -1,13 +1,24 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { HttpStatusCode } from 'axios';
 import { scrapOutfitItem } from '../../api/scrap';
+import { getServerErrorResponse } from '../../utils/error';
 
 const useScrapOutfitItem = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (outfitItemId: number) => scrapOutfitItem(outfitItemId),
+    throwOnError: (error) => {
+      if (getServerErrorResponse(error)?.status === HttpStatusCode.Conflict) {
+        return false;
+      }
+      return true;
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['fetchOutfitPostList'] });
+      queryClient.invalidateQueries({
+        queryKey: ['fetchMyScrapOutfitPostList'],
+      });
     },
   });
 };
--- src/hooks/mutations/useScrapOutfitPost.ts
+++ src/hooks/mutations/useScrapOutfitPost.ts
@@ -1,13 +1,24 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { HttpStatusCode } from 'axios';
 import { scrapOutfitPost } from '../../api/scrap';
+import { getServerErrorResponse } from '../../utils/error';
 
 const useScrapOutfitPost = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (outfitPostId: number) => scrapOutfitPost(outfitPostId),
+    throwOnError: (error) => {
+      if (getServerErrorResponse(error)?.status === HttpStatusCode.Conflict) {
+        return false;
+      }
+      return true;
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['fetchOutfitPostList'] });
+      queryClient.invalidateQueries({
+        queryKey: ['fetchMyScrapOutfitPostList'],
+      });
     },
   });
 };
