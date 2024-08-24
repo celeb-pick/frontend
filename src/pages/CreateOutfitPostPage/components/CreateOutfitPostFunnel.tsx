86409f8a32f9114b38e92edb7238b494abc055b4
Eunchae Kim <kec9706@gmail.com>
2024-08-24 14:59:46 +0900

    feat: CreateOutfitPostFunnel 컨테이너 스타일 적용

--- src/pages/CreateOutfitPostPage/components/CreateOutfitPostFunnel.tsx
+++ src/pages/CreateOutfitPostPage/components/CreateOutfitPostFunnel.tsx
@@ -1,13 +1,16 @@
+import tw from 'twin.macro';
 import useCreateOutfitPostPageContext from '../useCreateOutfitPostPageContext';
-import CreateOutfitPostCelebrityStep from './CreateOutfitPostCelebrityStep/CreateOutfitPostCelebrityStep';
+import CreateOutfitPostCelebrityStep from './CreateOutfitPostCelebrityStep';
 
 function CreateOutfitPostFunnel() {
   const { Funnel } = useCreateOutfitPostPageContext();
 
   return (
-    <Funnel>
-      <CreateOutfitPostCelebrityStep />
-    </Funnel>
+    <div css={[tw`flex flex-col items-center flex-1 p-4 pb-6`]}>
+      <Funnel>
+        <CreateOutfitPostCelebrityStep />
+      </Funnel>
+    </div>
   );
 }
 
